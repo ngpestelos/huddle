1b0750e71fd6fb6a78bd95c1d54c037dae1e9973
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2014-04-13 19:44:08 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Huddle::Application.config.secret_key_base = 'fc50bf4b67796f90dc5d6c4b87a1ea2f4b3e9a86732b33bda29d43b3d52b56309c0fe5867a11deb1e2a5d68ed2c2f21670d91886942d78bddcd406330eda188b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Huddle::Application.config.session_store :cookie_store, key: '_huddle_session'
