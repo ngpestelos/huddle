6bdfeaaffc7740ff4e67a4ccdce56a1ddda205ef
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2014-04-13 21:04:06 +0800

    add tests for StatusReport (default status_date)

--- app/models/status_report.rb
+++ app/models/status_report.rb
@@ -1,4 +1,12 @@
 class StatusReport < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
+
+  before_save :set_status_date
+
+  private
+  
+  def set_status_date
+    self.status_date = Date.today if status_date.nil?
+  end
 end
